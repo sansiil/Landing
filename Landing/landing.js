53eda54d4b51e1f75a37e50ca9390724cf2f8e18
sansiil <121335291+sansiil@users.noreply.github.com>
2023-01-04 22:58:48 +0545

    Add files via upload

---
+++ Landing/landing.js
@@ -0,0 +1,14 @@
+
+const drop= document.getElementById('dropdown');
+
+function navbar(){
+
+    if(drop.style.display === 'none'){
+        drop.style.display= 'block';
+        
+    }
+    else{
+        drop.style.display= 'none';
+    }
+
+};
